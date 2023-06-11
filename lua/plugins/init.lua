4051dc19da36e664a18ea6e34591dc08f0a27e49
hwakachan <hi_wakachan0823@icloud.com>
2023-06-11 14:19:33 +0900

    config nvim-cmp

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -20,4 +20,13 @@ return require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim',
     requires = { {'nvim-lua/plenary.nvim'} }
 }
+  use 'neovim/nvim-lspconfig'
+  use'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-vsnip'
+  use 'hrsh7th/vim-vsnip'
+  use 'onsails/lspkind-nvim'
 end)
--- lua/whichkey-config/init.lua
+++ lua/whichkey-config/init.lua
@@ -6,7 +6,19 @@ local mappings = {
   x = {":bdelete<cr>", "Close"},
   E = {":e ~/.config/nvim/init.lua", "Edit config"},
   f = {":Telescope find_files<cr>", "Telescope Find Files"},
-  r = {":Telescope live_grep<cr>", "Telescope Live Grep"}
+  r = {":Telescope live_grep<cr>", "Telescope Live Grep"},
+  l = {
+    name = "LSP",
+    i = {":LspInfo<cr>", "Connected Language Servers"},
+    k = {'<cmd>lua vim.lsp.buf.signature_help()<CR>', "Signature help"},
+    K = {'<cmd>lua vim.lsp.buf.hover()<CR>', "Hover"},
+    w = {'<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', "Add workspace folder"},
+    W = {'<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', "Remove workspace folder"},
+    l = {
+      '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
+      "List workspace folder"
+    }
+  }
 }
 local opts = { prefix = '<leader>'}
 wk.register(mappings, opts)
